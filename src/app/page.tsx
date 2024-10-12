fa9e4f33e78a3323f6858f9505f139ef36b5381f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-12 21:24:08 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import Sketch from '../components/js/MainShader';
+import Card from "@/components/Cards";
 
 
 export default function Home() {
@@ -14,11 +15,11 @@ export default function Home() {
   })
   return (
     <>
-      <div ref={ref} className="absolute top-0 left-0 w-full h-screen">
-      </div>
+      <div ref={ref} className="absolute top-0 left-0 w-full h-screen"></div>
       <section id="hero" className="hero h-screen relative flex flex-col items-center justify-center">
-        <h1 className="text-[8vw] text-center w-[65%] mx-auto leading-none">Create with <span className="italic">Firefly</span> generative AI.</h1>
+        <h1 className="text-[8vw] text-center w-[75%] mx-auto leading-none ">Create with <span className="italic">Firefly</span> generative AI.</h1>
       </section>
+      <Card />
     </>
     
   );
