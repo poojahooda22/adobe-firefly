65124392ff09592a55c00abca5375885c249de0b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-12 21:24:20 +0530

    created card

---
+++ src/components/CardAnimSection.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CardAnimSection = () => {
+  return (
+    <section className="cards-section text-black relative bg-[#050505] flex flex-col sm:flex-row justify-center items-center nax-sn: py-[5vh] sm:h-screen w-full">
+    </section>
+  )
+}
+
+export default CardAnimSection
