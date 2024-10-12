81ec4fd6c8279cf342f3ae4d6dac57b6663c861c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-12 20:00:07 +0530

    added class name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
     <>
       <div ref={ref} className="absolute top-0 left-0 w-full h-screen">
       </div>
-      <section id="hero" class="hero h-screen relative flex flex-col items-center justify-center">
-        <h1 class="text-[8vw] text-center w-[65%] mx-auto leading-none">Create with <span class="italic">Firefly</span> generative AI.</h1>
+      <section id="hero" className="hero h-screen relative flex flex-col items-center justify-center">
+        <h1 className="text-[8vw] text-center w-[65%] mx-auto leading-none">Create with <span className="italic">Firefly</span> generative AI.</h1>
       </section>
     </>
     
