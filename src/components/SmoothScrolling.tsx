15554e5c67e77cb2bceb84282cc5f227836671d4
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-12 21:58:00 +0530

    created smooth scrolling

---
+++ src/components/SmoothScrolling.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { ReactLenis } from "@studio-freight/react-lenis";
+
+function SmoothScrolling({ children }: React.PropsWithChildren<object>) {
+  return (
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5 }}>
+      {children}
+    </ReactLenis>
+  );
+}
+
+export default SmoothScrolling;
