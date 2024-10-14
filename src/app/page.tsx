00fbdb70cad85450ff898344c3a9d85261c0916b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-10-14 09:54:04 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   })
   return (
     <>
-      <div ref={ref} className="absolute top-0 left-0 w-full h-screen"></div>
+      <div ref={ref} className="absolute top-0 left-0 w-full h-screen hue-rotate-320"></div>
       <section id="hero" className="hero h-screen relative flex flex-col items-center justify-center">
         <h1 className="text-[8vw] text-center w-[75%] mx-auto leading-none ">Create with <span className="italic">Firefly</span> generative AI.</h1>
       </section>
